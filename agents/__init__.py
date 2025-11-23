b3865acdd5d655c12328452f26e63fb8a14505a1
alexalkw <146472597+alexalkw@users.noreply.github.com>
2025-11-23 15:49:38 -0500

    adding to init

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,4 +1,6 @@
 from .random_agent import RandomAgent
 from .human_agent import HumanAgent
-from .student_agent import StudentAgent
 from .greedy_corners_agent import StudentAgent
+from .student_agent import StudentAgent
+from .greedy_heuristic import StudentAgent
+
